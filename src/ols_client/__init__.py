93a4f0fdd541de19390397d237d5b7a2d327c72d
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-10-01 00:10:43 +0200

    Bump version: 0.0.7-dev → 0.0.7

--- src/ols_client/__init__.py
+++ src/ols_client/__init__.py
@@ -19,7 +19,7 @@ __all__ = (
     api.__all__
 )
 
-__version__ = '0.0.7-dev'
+__version__ = '0.0.7'
 
 __title__ = 'ols_client'
 __description__ = 'A Python client for the EBI Ontology Lookup Service RESTful API'
